5b62cb910d243507333b317fcadc389e060e4ae7
Sneden Valen Diniz <sdiniz@citiscapegroup.com>
2024-08-08 10:31:46 +0400

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 var slideIndex = 0;
-        carousel();
 
-        function carousel() {
-          var i;
-          var x = document.getElementsByClassName("mySlides");
-          for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none";
-          }
-          slideIndex++;
-          if (slideIndex > x.length) {slideIndex = 1}
-          x[slideIndex-1].style.display = "block";
-          setTimeout(carousel, 2000); // Change image every 2 seconds
-        }
+carousel();
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1;}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
 function App() {
   return (
     <div className="App">
@@ -23,11 +23,6 @@ function App() {
         <h1>CITISCAPES EVENTS</h1>
         <img class="mySlides" src="https://e1.pxfuel.com/desktop-wallpaper/844/928/desktop-wallpaper-ferrari-world-abu-dhabi.jpg"></img>
         <img class="mySlides" src="https://www.citiscapegroup.com/wp-content/uploads/2021/05/shutterstock_191551838-1.jpeg"></img>
-        <img class="mySlides" src="img_mountains.jpg"></img>
-        <img class="mySlides" src="img_forest.jpg"></img>
-        <button class="w3-button w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
-        <button class="w3-button w3-display-right" onclick="plusDivs(+1)">&#10095;</button>
-        
       </body>
     </div>
   );
