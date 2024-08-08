277d77a1deac2e9e94a3a89d6c44ab630b2b554d
abiiyer224 <abiiyer224@gmail.com>
2024-08-08 10:00:42 +0400

    welcome page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+var slideIndex = 0;
+        carousel();
 
+        function carousel() {
+          var i;
+          var x = document.getElementsByClassName("mySlides");
+          for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+          }
+          slideIndex++;
+          if (slideIndex > x.length) {slideIndex = 1}
+          x[slideIndex-1].style.display = "block";
+          setTimeout(carousel, 2000); // Change image every 2 seconds
+        }
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body> 
+        <h1>CITISCAPES EVENTS</h1>
+        <img class="mySlides" src="https://e1.pxfuel.com/desktop-wallpaper/844/928/desktop-wallpaper-ferrari-world-abu-dhabi.jpg"></img>
+        <img class="mySlides" src="https://www.citiscapegroup.com/wp-content/uploads/2021/05/shutterstock_191551838-1.jpeg"></img>
+        <img class="mySlides" src="img_mountains.jpg"></img>
+        <img class="mySlides" src="img_forest.jpg"></img>
+        <button class="w3-button w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+        <button class="w3-button w3-display-right" onclick="plusDivs(+1)">&#10095;</button>
+        
+      </body>
     </div>
   );
 }
