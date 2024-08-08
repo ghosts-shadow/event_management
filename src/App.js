8f321834effe63093ec3aec6ce398c6fe5274eb8
abiiyer224 <abiiyer224@gmail.com>
2024-08-08 13:54:29 +0400

    slideshow

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { useState } from 'react';
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Carousel from 'react-bootstrap/Carousel';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 function App() {
   const [index, setIndex] = useState(0);
 
@@ -22,9 +26,17 @@ function App() {
             <img class="mySlides" src="https://www.citiscapegroup.com/wp-content/uploads/2021/05/shutterstock_191551838-1.jpeg"></img>
           </Carousel.Item>
         </Carousel>
+        <Container>
+          <Row xs={2} md={4} lg={6}>
+            <Col><img class="hi" src="https://www.citiscapegroup.com/wp-content/uploads/2023/12/SAfety-.jpg"></img></Col>
+            <Col>2 of 2</Col>
+          </Row>
+
+      </Container>
       </body>
     </div>
   );
 }
+
 
 export default App;
